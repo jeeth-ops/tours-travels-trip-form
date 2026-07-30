0af65349daf160616cb922ea72437bb6b7210946
jeeth-ops <chhayajeeth@gmail.com>
2026-07-30 09:29:50 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,29 @@
+const CACHE_NAME = 'tours-portal-v2'; // Jab bhi bada update karein, isko v3, v4 kar dena
+const ASSETS = [
+  './',
+  'index.html'
+];
+
 self.addEventListener('install', (e) => {
+  self.skipWaiting(); // Naye service worker ko bina wait kiye turant activate karega
   e.waitUntil(
-    caches.open('tours-portal-v1').then((cache) => {
-      return cache.addAll([
-        './',
-        'index.html'
-      ]);
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
     })
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key); // Purane sabhi old caches ko automatic saaf kar dega
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
   );
 });
 
