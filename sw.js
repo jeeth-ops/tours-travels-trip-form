96d468d4bfc9825c8fb553c54f31f06e9d4dd94f
jeeth-ops <chhayajeeth@gmail.com>
2026-08-14 01:51:50 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'tours-portal-v4'; // Jab bhi bada update karein, isko v5, v6 kar dena
+const CACHE_NAME = 'tours-portal-v6'; // Jab bhi bada update karein, isko v7, v8 kar dena
 const ASSETS = [
   './',
   'index.html'
